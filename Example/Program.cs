cb6eb4e09e2929cf7e41c1e4f1932a0e5a32656c
Adithya Vijay K <adithyavj@gmail.com>
2021-07-26 15:19:56 +0530

    initial commit

---
+++ Example/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuerySyntax();
+            Console.ReadLine();
+        }
+
+        private static void QuerySyntax()
+        {
+            // Data Source
+            List<int> integerList = new List<int>()
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+            };
+
+            // LINQ Query Syntax
+            var result = from obj in integerList
+                         where obj > 5
+                         select obj;
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item + " ");
+            }
+        }
+    }
+}
